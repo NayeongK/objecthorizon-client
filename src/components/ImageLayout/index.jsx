bcca3296940925f33d037ebb96401fe94534e04a
nayeong <nyk1118@naver.com>
2023-08-25 02:18:01 +0900

    feature: Add zoom in/out effects based on mouse position

--- src/components/ImageLayout/index.jsx
+++ src/components/ImageLayout/index.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
 import { fetchAllBackgroundImages } from "../../utils/fetchImages";
-import { getDominantBackgroundColor } from "../../utils/getBackgroundColor";
-import { sendDominantColorToServer } from "../../utils/sendColorToServer";
+import { throttle } from "lodash";
 
 function ImageLayout() {
   const canvasRef = useRef(null);
   const [images, setImages] = useState([]);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [zoom, setZoom] = useState(1);
-  const [startPinchDistance, setStartPinchDistance] = useState(null);
-  const [startZoom, setStartZoom] = useState(null);
-  const [processedImages, setProcessedImages] = useState({});
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const [viewState, setViewState] = useState({ imageIndex: 0, zoom: 1 });
 
   useEffect(() => {
     async function loadImages() {
@@ -20,140 +17,97 @@ function ImageLayout() {
     loadImages();
   }, []);
 
-  useEffect(() => {
-    if (zoom > 3) {
-      goToNextImage();
-    } else if (zoom < 0.3) {
-      goToPreviousImage();
-    }
-  }, [zoom]);
-
-  useEffect(() => {
-    if (images.length > 0) {
-      processAllImages();
-    }
-  }, [images]);
-
-  async function processAllImages() {
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-
-    images.forEach(async (imgSrc) => {
-      if (!processedImages[imgSrc]) {
-        const image = new Image();
-        image.crossOrigin = "anonymous";
-
-        image.addEventListener("load", async () => {
-          canvas.width = image.width;
-          canvas.height = image.height;
-          ctx.drawImage(image, 0, 0);
-          const dominantBackgroundColor = getDominantBackgroundColor(canvas);
-          await sendDominantColorToServer(imgSrc, dominantBackgroundColor);
-          setProcessedImages((prevProcessedImages) => ({
-            ...prevProcessedImages,
-            [imgSrc]: true,
-          }));
-        });
-
-        image.src = imgSrc;
-      }
-    });
-  }
-
   function goToNextImage() {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    setZoom(1);
+    setViewState((prevState) => ({
+      imageIndex: (prevState.imageIndex + 1) % images.length,
+      zoom: 1,
+    }));
   }
 
   function goToPreviousImage() {
-    setCurrentImageIndex((prevIndex) => {
-      return (prevIndex - 1 + images.length) % images.length;
-    });
-    setZoom(1);
+    setViewState((prevState) => ({
+      imageIndex: (prevState.imageIndex - 1 + images.length) % images.length,
+      zoom: 1,
+    }));
   }
 
-  function drawImage(canvas, ctx, image, zoom) {
+  function drawImage(canvas, ctx, image, zoomValue) {
     const canvasWidth = window.innerWidth;
     const canvasHeight = window.innerHeight;
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
     const imgRatio = image.width / image.height;
-    const canvasRatio = canvasWidth / canvasHeight;
-    let drawWidth, drawHeight;
+    let drawWidth = canvasWidth * zoomValue;
+    let drawHeight = drawWidth / imgRatio;
 
-    if (imgRatio > canvasRatio) {
-      drawWidth = canvasWidth * zoom;
-      drawHeight = drawWidth / imgRatio;
-    } else {
-      drawHeight = canvasHeight * zoom;
-      drawWidth = drawHeight * imgRatio;
-    }
+    const offsetX = (mousePosition.x - canvasWidth / 2) * (zoomValue - 1);
+    const offsetY = (mousePosition.y - canvasHeight / 2) * (zoomValue - 1);
 
-    const startX = (canvasWidth - drawWidth) / 2;
-    const startY = (canvasHeight - drawHeight) / 2;
+    const startX = (canvasWidth - drawWidth) / 2 - offsetX;
+    const startY = (canvasHeight - drawHeight) / 2 - offsetY;
 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     ctx.drawImage(image, startX, startY, drawWidth, drawHeight);
   }
 
-  function calculatePinchDistance(touches) {
-    const [touch1, touch2] = touches;
-    const dx = touch1.clientX - touch2.clientX;
-    const dy = touch1.clientY - touch2.clientY;
-    return Math.sqrt(dx * dx + dy * dy);
-  }
-
-  function handleTouchStart(e) {
-    if (e.touches.length === 2) {
-      const distance = calculatePinchDistance(e.touches);
-      setStartPinchDistance(distance);
-      setStartZoom(zoom);
-      e.preventDefault();
-    }
-  }
-
-  function handleTouchMove(e) {
-    if (e.touches.length === 2 && startPinchDistance !== null) {
-      const distance = calculatePinchDistance(e.touches);
-      const scale = distance / startPinchDistance;
-      setZoom(Math.max(0.1, startZoom * scale));
-      e.preventDefault();
-    }
-  }
-
-  function handleTouchEnd(e) {
-    setStartPinchDistance(null);
-    setStartZoom(null);
-  }
-
   function handleWheel(e) {
-    const scale = -e.deltaY * 0.01;
-    setZoom((prevZoom) => Math.max(0.1, prevZoom + scale));
+    let scale = -e.deltaY * 0.01;
+    let currentZoom = viewState.zoom;
+    const maxZoom = 400;
+    const minZoom = 0.3;
+
+    if (currentZoom * (1 + scale) > maxZoom) {
+      goToNextImage();
+      e.preventDefault();
+      return;
+    } else if (currentZoom * (1 + scale) < minZoom) {
+      goToPreviousImage();
+      e.preventDefault();
+      return;
+    }
+
+    const newZoom = currentZoom * (1 + scale);
+
+    setViewState((prevState) => ({
+      ...prevState,
+      zoom: newZoom,
+    }));
     e.preventDefault();
   }
 
+  const throttleHandler = throttle(handleWheel, 800);
+
   useEffect(() => {
     const canvas = canvasRef.current;
-    if (canvas) {
-      canvas.addEventListener("touchstart", handleTouchStart, {
-        passive: false,
-      });
-      canvas.addEventListener("touchmove", handleTouchMove, { passive: false });
-      canvas.addEventListener("touchend", handleTouchEnd, { passive: false });
-      canvas.addEventListener("wheel", handleWheel, { passive: false });
+
+    function handleMouseMove(e) {
+      setMousePosition({ x: e.clientX, y: e.clientY });
     }
 
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("wheel", throttleHandler, { passive: false });
+
     return () => {
-      if (canvas) {
-        canvas.removeEventListener("touchstart", handleTouchStart);
-        canvas.removeEventListener("touchmove", handleTouchMove);
-        canvas.removeEventListener("touchend", handleTouchEnd);
-        canvas.removeEventListener("wheel", handleWheel);
-      }
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("wheel", throttleHandler);
     };
-  }, [startPinchDistance, startZoom, zoom]);
+  }, [viewState.zoom]);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+
+    image.addEventListener("load", () => {
+      drawImage(canvas, ctx, image, viewState.zoom);
+    });
+
+    if (images.length > 0) {
+      image.src = images[viewState.imageIndex];
+    }
+  }, [images, viewState]);
 
   return (
     <canvas
