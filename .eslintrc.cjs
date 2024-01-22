a4fc7e3e7e39a17366b9a6ad44c2455ae61790ce
nayeong <nyk1118@naver.com>
2024-01-22 09:32:16 +0900

    chore: set eslint plugin import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,11 +11,53 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "unknown",
+        ],
+        pathGroups: [
+          {
+            pattern: "react*",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@hooks/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@pages/*",
+            group: "internal",
+            position: "after",
+          },
+          {
+            pattern: "@components/*",
+            group: "internal",
+            position: "after",
+          },
+        ],
+
+        pathGroupsExcludedImportTypes: ["@tanstack*"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
     ],
   },
 };
